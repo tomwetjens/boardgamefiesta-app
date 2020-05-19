2ff698945364368bd67fcffcbea5e22293852025
Tom Wetjens <tomwetjens@gmail.com>
2020-05-19 10:37:57 +0200

    update cognito url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,17 +2,17 @@ import {Environment} from './environment.model';
 
 export const environment: Environment = {
   production: true,
-  apiBaseUrl: 'https://gwt-api.wetjens.com',
-  wsBaseUrl: 'wss://gwt-api.wetjens.com',
+  apiBaseUrl: 'https://api.tomsboardgames.com',
+  wsBaseUrl: 'wss://api.tomsboardgames.com',
   auth: {
     issuer: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_c5qwAzc0x',
-    loginUrl: 'https://gwt.auth.eu-west-1.amazoncognito.com/oauth2/authorize',
-    tokenEndpoint: 'https://gwt.auth.eu-west-1.amazoncognito.com/oauth2/token',
+    loginUrl: 'https://auth.tomsboardgames.com/oauth2/authorize',
+    tokenEndpoint: 'https://auth.tomsboardgames.com/oauth2/token',
     clientId: '39334dh8ifidq5dpmnch2vnopr',
     responseType: 'code',
     scope: 'openid profile',
     oidc: true,
     redirectUri: window.location.origin,
-    logoutUrl: 'https://gwt.auth.eu-west-1.amazoncognito.com/logout?client_id=39334dh8ifidq5dpmnch2vnopr&logout_uri=' + window.location.origin
+    logoutUrl: 'https://auth.tomsboardgames.com/logout?client_id=39334dh8ifidq5dpmnch2vnopr&logout_uri=' + window.location.origin
   }
 };
