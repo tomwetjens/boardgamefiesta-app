54f5923133103c9b02df0ec87e974771c907f6af
Tom Wetjens <tomwetjens@gmail.com>
2020-07-13 15:17:34 +0200

    set title

--- src/app/games/game/game.component.ts
+++ src/app/games/game/game.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {GameService} from "../../game.service";
 import {switchMap} from "rxjs/operators";
 import {Observable} from "rxjs";
 import {Game, TableMode, TableType} from "../../shared/model";
 import {TableService} from "../../table.service";
+import {Title} from "@angular/platform-browser";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-game',
@@ -15,8 +17,11 @@ export class GameComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private router: Router,
+              private title: Title,
+              private translateService: TranslateService,
               private gameService: GameService,
-              private tableService: TableService) {}
+              private tableService: TableService) {
+  }
 
   game: Observable<Game>;
 
@@ -25,7 +30,10 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     this.game = this.route.params
       .pipe(switchMap(params => {
-        console.log(params);
+        const name = this.translateService.instant('game.' + params.id + '.name');
+        if (name) {
+          this.title.setTitle(name);
+        }
         return this.gameService.get(params.id);
       }));
   }
