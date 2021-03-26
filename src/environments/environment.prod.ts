88050384d989173e6f46522ad0b437b042242872
Tom Wetjens <tomwetjens@gmail.com>
2021-03-26 15:19:00 +0100

    Switch to new user pool

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Environment = {
   apiBaseUrl: 'https://api.boardgamefiesta.com',
   wsBaseUrl: 'wss://api.boardgamefiesta.com',
   auth: {
-    issuer: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_c5qwAzc0x',
-    loginUrl: 'https://auth.boardgamefiesta.com/oauth2/authorize',
-    tokenEndpoint: 'https://auth.boardgamefiesta.com/oauth2/token',
-    clientId: '39334dh8ifidq5dpmnch2vnopr',
+     issuer: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_4FuZdxo27',
+    loginUrl: 'https://auth2.boardgamefiesta.com/oauth2/authorize',
+    tokenEndpoint: 'https://auth2.boardgamefiesta.com/oauth2/token',
+    clientId: '75mphvul79khilegesecif6o0k',
     responseType: 'code',
     scope: 'openid profile',
     oidc: true,
     redirectUri: window.location.origin,
-    logoutUrl: 'https://auth.boardgamefiesta.com/logout?client_id=39334dh8ifidq5dpmnch2vnopr&logout_uri=' + window.location.origin
+    logoutUrl: 'https://auth2.boardgamefiesta.com/logout?client_id=75mphvul79khilegesecif6o0k&logout_uri=' + window.location.origin
   }
 };
