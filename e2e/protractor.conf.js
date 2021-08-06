4bcc7439384b9325e682f27aa3b36f63ac870c2d
Tom Wetjens <tomwetjens@gmail.com>
2021-08-06 16:53:53 +0200

    Add copyright header

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,3 +1,21 @@
+/*
+ * Board Game Fiesta
+ * Copyright (C)  2021 Tom Wetjens <tomwetjens@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
